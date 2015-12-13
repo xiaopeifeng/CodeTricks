3cb48501677e8658b49618af5c8b61661d839dd9
xiaopeifeng <xiaopeifenng@gmail.com>
2015-12-13 14:22:08 +0800

    modify

--- switch_use.cc
+++ switch_use.cc
@@ -1,6 +1,7 @@
 #include <iostream>
-// 使用这种switch case的方法，在某些简单的场景下可以减少if语句的使用，极大的增加了程序的观赏性和可读性
-// 这里只是给出一种思路，来减少if语句的使用
+/// a better use of switch which can decrease the number
+/// of 'if' occurence
+///
 
 bool is_tspecial(int c)
 {
@@ -16,6 +17,9 @@ bool is_tspecial(int c)
 int main()
 {
 	char c = ' ';
-	if(is_tspecial(c)) std::cout << "yes" << std::endl;
-	else std::cout << "no" << std::endl;
+	if(is_tspecial(c)){
+		 std::cout << "yes" << std::endl;
+	}else{ 
+		std::cout << "no" << std::endl;
+	}
 }
